3ab9a75842833cf61cd9458a087b3f2e2fbc5642
Karan Patel <kpatel5797@gmail.com>
2018-04-03 21:20:33 -0400

    init commit - Maven, Spring

---
+++ src/main/java/com/marketstructure/MarketStructureApplication.java
@@ -0,0 +1,12 @@
+package com.marketstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketStructureApplication.class, args);
+	}
+}
